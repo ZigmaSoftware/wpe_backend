a66697fc8fbdabb70aa7334dfc24a4e2656134fe
ZigmaSoftware <creative@zigma.in>
2026-04-24 13:37:34 +0530

    pull code from dev backend

--- MASTERS/Items/urls.py
+++ MASTERS/Items/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import ItemViewSet
 
-router = DefaultRouter()
-router.register(r'items', ItemViewSet)
+item_list = ItemViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+item_import = ItemViewSet.as_view({
+    'post': 'import_excel',
+})
+
+item_detail = ItemViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
 
 urlpatterns = [
-    path('items', include(router.urls)),
+    path('items/', item_list, name='item-list'),
+    path('items/import/', item_import, name='item-import'),
+    path('items/<int:pk>/', item_detail, name='item-detail'),
 ]
