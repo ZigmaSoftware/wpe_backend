e534dc563c57e35a7aade9c17b04aa1767eeb51c
ZigmaSoftware <creative@zigma.in>
2026-06-09 18:09:34 +0530

    password issue fixed

---
+++ core_system/apps/admin_master/migrations/0012_merge_0011_admin_master_branches.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("admin_master", "0011_move_blending_store_request_to_requests_workspace"),
+        ("admin_master", "0011_usercreation_password"),
+    ]
+
+    operations = []
